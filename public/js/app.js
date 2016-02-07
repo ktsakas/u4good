bf0f2173cd7a83e13ebe3b71055e37a2e9f8fdff
Konstantinos Tsakas <kostastsakas@gmail.com>
2016-02-06 21:41:17 -0500

    added facebook login

--- public/js/app.js
+++ public/js/app.js
@@ -1,6 +1,93 @@
-var app = angular.module('u4goodApp', ['firebase']);
+var app = angular.module('u4goodApp', ['ngRoute', 'firebase']);
 
-app.controller('mainCtrl', ['$scope', '$firebaseObject', function($scope, $firebaseObject) {
+/*app.controller('menuController', ['$scope', 'menuItems', '$location', function($scope, menuItems, $location) {
+	$scope.menuItems = menuItems;
+
+	$scope.isSelected = function(menuItem) {
+		return menuItem.path === $location.path();
+	};
+
+	$scope.click = function(menuItem) {
+		$location.path(menuItem.path);
+	};
+}]);*/
+
+app.config(['$routeProvider', function($routeProvider) {
+	$routeProvider.when('/', {
+		templateUrl: 'partials/goals.html',
+		controller: 'goalsCtrl'
+	})
+	.when('/newGoal', {
+		templateUrl: 'partials/new_goal.html',
+		controller: 'newGoalCtrl'
+	})
+	.when('/goals', {
+		templateUrl: 'partials/goals.html',
+		controller: 'goalsCtrl'
+	});
+	
+	// $locationProvider.hashPrefix('!');
+	// $locationProvider.html5Mode(true);
+}]);
+
+app.controller('mainCtrl', function ($scope) {
+	var ref = new Firebase("https://u4good.firebaseio.com");
+
+	// Check if the user is logged in
+	if (ref.getAuth()) {
+		$scope.loggedIn = true;
+	}
+
+	$scope.SignUp = function () {
+		// If the user is not logged in
+		if ($scope.loggedIn) return;
+
+		// Open facebook login popup
+		ref.authWithOAuthPopup("facebook", function(error, authData) {
+			if (error) {
+				console.log("Login Failed!", error);
+				$scope.loggedIn = false;
+			} else {
+				console.log("Authenticated successfully with payload:", authData);
+				$scope.loggedIn = true;
+				$scope.$apply();
+			}
+		});
+	};
+});
+
+app.controller('menuCtrl', function ($scope) {});
+
+app.controller('signUpCtrl', function ($scope) {
+	console.log("how controller run");
+});
+
+app.controller('howCtrl', function ($scope) {
+	console.log("how controller run");
+});
+
+app.controller('newGoalCtrl', function ($scope) {
+	console.log("new goal run");
+});
+
+app.controller('goalsCtrl', function ($scope) {
+	$scope.goals = [
+		{'goal': 'Lose Weight',
+		'money': '50$',
+		'time': 2,
+		'organization': 'Unicef'},
+		{'goal': 'Win the lottery',
+		'money': '10$',
+		'time': 1,
+		'organization': 'Gamblers Association'},
+		{'goal': 'Go to the moon',
+		'money': '2$',
+		'time': 2,
+		'organization': 'NASA'}
+	];
+});
+
+/*app.controller('mainCtrl', ['$scope', '$firebaseObject', function($scope, $firebaseObject) {
 	// console.log('hello world');
 	// $scope.foo = 'bar';
 	// $scope.goo = 'zerp';
@@ -59,4 +146,4 @@ app.controller('mainCtrl', ['$scope', '$firebaseObject', function($scope, $fireb
 app.controller('nestedCtrl', ['$scope', function($scope) {
 	// console.log('hello inner world');
 	// $scope.foo = 'derp';
-}]);
+}]);*/
